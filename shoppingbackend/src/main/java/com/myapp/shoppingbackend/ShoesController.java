25d095f65783e454d64265f65bb5e1ccfdf9ab38
ESUM BRANDON <esumbrandon074@gmail.com>
2024-04-10 15:09:02 +0100

    init pro

--- shoppingbackend/src/main/java/com/myapp/shoppingbackend/ShoesController.java
+++ shoppingbackend/src/main/java/com/myapp/shoppingbackend/ShoesController.java
@@ -1,12 +1,11 @@
 package com.myapp.shoppingbackend;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/shoes")
