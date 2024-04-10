0c53f2c33ae153e09abc1df6b47d6a280dbf5178
ESUM BRANDON <esumbrandon074@gmail.com>
2024-04-10 15:05:12 +0100

    API dev init

---
+++ shoppingbackend/src/main/java/com/myapp/shoppingbackend/ShoppingbackendApplication.java
@@ -0,0 +1,15 @@
+package com.myapp.shoppingbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.List;
+
+@SpringBootApplication
+public class ShoppingbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingbackendApplication.class, args);
+	}
+
+}
